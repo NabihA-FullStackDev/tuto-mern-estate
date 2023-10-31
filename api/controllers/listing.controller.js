2aa892b75bfe48f3a64764f5c2c14aab68003888
Nabih ALI <nabih@buga.fr>
2023-11-01 00:28:39 +0100

    Create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -12,11 +13,30 @@ export const createListing = async (req, res, next) => {
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) return next(errorHandler(404, "Listing not found"));
-  if (req.user.id !== listing.userRef) return next(errorHandler(401, "Unauthorized"));
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "Unauthorized"));
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) return next(errorHandler(404, "Listing not found"));
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "Unauthorized"));
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // Sans ça l'ancien sera retourné plutot que la nouvelle version
+    );
+    res.status(200).json("Listing has been updated successfully");
   } catch (error) {
     next(error);
   }
